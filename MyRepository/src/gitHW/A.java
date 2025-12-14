d9e322c31536e071f079576af92868cd6bb6b1d4
김소희 <paa328@naver.com>
2025-12-14 17:45:47 +0900

    처음 만든 거

---
+++ MyRepository/src/gitHW/A.java
@@ -0,0 +1,11 @@
+package gitHW;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("A class");
+
+	}
+
+}
---
+++ MyRepository/src/gitHW/C.java
@@ -0,0 +1,10 @@
+package gitHW;
+
+public class C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("C class");
+	}
+
+}
